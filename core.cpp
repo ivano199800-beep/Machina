a394102b6ee3893af6d67b802c99bcca643dce09
unknown <ivano199800@gmail.com>
2026-05-07 02:27:15 +0800

    added: instructions (ADD , SUB , SND , RCV)

--- core.cpp
+++ core.cpp
@@ -27,7 +27,7 @@ void core16::tick() {
             HWORD_ opcode   = (instr >> 11) & 0x1f;
             HWORD_ opmode   = (instr >> 8) & 0x7;
             HWORD_ regB     = (instr >> 5) & 0x7;
-            HWORD_ regA     = (instr >> 2) & 0x3;
+            HWORD_ regA     = (instr >> 2) & 0x7;
             HWORD_ rMode    = instr & 0x3;
             
             switch ((isa)opcode) {
@@ -36,6 +36,18 @@ void core16::tick() {
                 case isa::HLT:                    
                     this->Send(0 , 1);
                     this->Send(1 , 1);
+                    break;
+                case isa::ADD:
+                    this->regx[regB] += this->regx[regB];
+                    break;
+                case isa::SUB:
+                    this->regx[regB] -= this->regx[regB];
+                    break;
+                case isa::SND:
+                    this->Send(regx[regB] , regx[regA]);
+                    break;
+                case isa::RCV:
+                    regx[regA] = this->Receive(regx[regB]);
                     break;
                 default:
                     break;
